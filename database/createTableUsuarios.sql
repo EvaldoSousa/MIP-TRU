fd8235a72f6601cc00742f52fea26f056b0cf40d
JhonnieRandler <jhon.randler@hotmail.com>
2021-04-14 18:17:00 -0300

    Adicionado botão de mostrar senha

--- database/createTableUsuarios.sql
+++ database/createTableUsuarios.sql
@@ -1,9 +1,11 @@
+-- Não rode pq ainda não tá definitivo
+
 CREATE TABLE usuarios
 (
-	id SERIAL PRIMARY KEY,
 	nomecompleto VARCHAR(100) NOT NULL,
 	email VARCHAR(50) NOT NULL,
 	nomeusuario VARCHAR(50) NOT NULL,
 	telefone VARCHAR(12) NOT NULL,
-	senha VARCHAR(100) NOT NULL
+	senha VARCHAR(100) NOT NULL,
+	PRIMARY KEY(email, nomeusuario)
 );
