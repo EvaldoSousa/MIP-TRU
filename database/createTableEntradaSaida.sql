e4025b7d6788f3d7df3a1c5f1887dbc678554d22
Evaldo Sousa <fevaldo57@gmail.com>
2021-05-31 01:16:09 -0300

    master

--- database/createTableEntradaSaida.sql
+++ database/createTableEntradaSaida.sql
@@ -1,4 +1,8 @@
-CREATE TABLE entradas(id BIGSERIAL PRIMARY KEY NOT NULL, ano VARCHAR(10) NOT NULL,
-					  entrada VARCHAR(100) NOT NULL,
-					  destinatario VARCHAR(100),
-					  cnae VARCHAR(300));
+CREATE TABLE entradas
+(
+	id BIGSERIAL PRIMARY KEY NOT NULL,
+	ano VARCHAR(50) NOT NULL,
+	entrada VARCHAR(100) NOT NULL,
+	destinatario VARCHAR(100),
+	cnae VARCHAR(300)
+);
