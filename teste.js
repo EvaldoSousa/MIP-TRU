f5619648e38cd46db2f9a1b3456ab11f71fc7b4d
JhonnieRandler <jhon.randler@hotmail.com>
2021-08-08 20:47:22 -0300

    função para separação de select multiples

--- teste.js
+++ teste.js
@@ -1,16 +1,16 @@
-function buscarSelect(entrada) {
-    let txt = 'SELECT DISTINCT ';
+original = "jubileu=opção1&jubileu=opção2&jubileu=opção3";
 
-    if(entrada == 'ano') {
-        txt += "ano FROM entradas order by ano asc";
-    }
-    if(entrada == 'destinatario') {
-        txt += "destinatario FROM entradas order by destinatario asc";
-    }
-    if(entrada == 'cnae') {
-        txt += "cnae FROM entradas order by cnae asc";
-    }
-    return txt;
-}
+// /&/g substitui todas as ocorrências de & por uma string vazia
+semEComercial = original.replace(/&/g, "");
 
-console.log(buscarSelect('destinatario'));
+// separa a string em um vetor, sempre que achar um "jubileu=" manda a string pra uma posição diferente do vetor
+stringCortada = semEComercial.split("jubileu=");
+
+//função pra remover as strings vazias
+stringFinal = stringCortada.filter(function (string) {
+    if (string != "") {
+        return string;
+    }
+})
+
+console.log(stringFinal);
