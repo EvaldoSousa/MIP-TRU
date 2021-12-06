c01c8fd345a366894a955b9b9bb665be0318d343
JoathanBR <joathanlopes@hotmail.com>
2021-12-06 17:59:03 -0300

    Corrigando a tabela entrada para entradas

--- migrations/20211119193749-create-entradas.js
+++ migrations/20211119193749-create-entradas.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('entrada', {
+    await queryInterface.createTable('entradas', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -103,6 +103,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('entrada');
+    await queryInterface.dropTable('entradas');
   }
 };
